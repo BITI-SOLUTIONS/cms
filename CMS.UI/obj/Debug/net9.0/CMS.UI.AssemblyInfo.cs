b19cd1bd0d78e53f85718c70954dc0e8ff465e14
Ernesto Martinez <ernesto.martinez@biti-solutions.com>
2026-02-26 18:03:21 -0600

    fix: mostrar botón Ejecutar Reporte y auto-ejecutar cuando no hay filtros

--- CMS.UI/obj/Debug/net9.0/CMS.UI.AssemblyInfo.cs
+++ CMS.UI/obj/Debug/net9.0/CMS.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a49f4d02c67bbaca612f7143fcda4cf3510ef50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708a9e27bf54399328113b95321dd574c050447f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
